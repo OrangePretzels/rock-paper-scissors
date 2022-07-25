35ef80b2abd2548d92e5dfef944c1b24bcf6fa0e
OrangePretzels <bamacomber8@gmail.com>
2022-07-25 15:45:44 -0400

    Updated with the results and cleaner code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
-console.log("game 1")
+
 function getComputerChoice(){
     let answer = Math.floor(Math.random()*3+1)
     if (answer === 1){
@@ -13,39 +13,42 @@ function getComputerChoice(){
     }
 }
 
-const playerSelection = prompt("Choose Rock Paper Scissors").toLowerCase();
-
-const computerSelection = getComputerChoice();
-
-console.log(playerSelection, computerSelection)
-
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
         return "it's a draw"
     } else if(playerSelection === "rock" && computerSelection === "scissors" ){
-        return "player wins rock beats scissors"
+        return "player wins rock beats scissors" + playerScore++
     } else if(playerSelection === "scissors" && computerSelection === "paper" ){
-        return "player wins scissors beats paper"
+        return "player wins scissors beats paper" + playerScore++
     } else if(playerSelection === "paper" && computerSelection === "rock" ){
-        return "player wins paper beats rock"
+        return "player wins paper beats rock" + playerScore++
     } else if (computerSelection === "rock" && playerSelection === "scissors" ){
-        return "computer wins rock beats scissors"
+        return "computer wins rock beats scissors" + computerScore++
     } else if (computerSelection === "scissors" && playerSelection === "paper" ){
-        return "computer wins scissors beats paper"
+        return "computer wins scissors beats paper" + computerScore++
     } else if (computerSelection === "paper" && playerSelection === "rock" ){
-        return "computer wins paper beats rock"
+        return "computer wins paper beats rock" + computerScore++
     }
     return "invalid value try again"
 }
-console.log(playRound(playerSelection, computerSelection));
 
 function game(){
-    for (let i = 2; i < 6; i++) {
-        let testone = prompt("Choose Rock Paper Scissors").toLowerCase();
-        let testtwo = getComputerChoice();
-        console.log("game " + i)
-        console.log(testone, testtwo)
-        console.log(playRound(testone, testtwo));
-     }
+    for (let i = 0; i < 5; i++) {
+        let playerInput = prompt("Choose Rock Paper Scissors").toLowerCase();
+        let computerInput = getComputerChoice();
+        console.log(playerInput, computerInput)
+        console.log(playRound(playerInput, computerInput));
+    }
 }
 game()
+
+function result(){
+if (playerScore === computerScore){
+    return "the series is tied at " + playerScore + " " + computerScore
+} else if (playerScore > computerScore){
+    return "you win the series " + playerScore + " " + computerScore
+} else if (computerScore > playerScore){
+    return "the computer wins the series " + computerScore + " " + playerScore
+}
+}
+console.log(result())
